a302cedcc24105f9a3bab5022c8040f528f57cf1
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-14 04:36:02 +0800

    Add login input forms

--- jwt-login/src/App.jsx
+++ jwt-login/src/App.jsx
@@ -3,6 +3,25 @@ import './styles/App.css'
 function App() {
   return (
     <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            required
+          />
+        </div>
+      </form>
     </div>
   );
 }
