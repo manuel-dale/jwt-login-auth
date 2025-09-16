0aba9fb7f62ae1aa8ddec65f190e64d3e73e4df4
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-16 09:49:10 +0800

    Add dotenv dependency

--- jwt-auth-backend/server.js
+++ jwt-auth-backend/server.js
@@ -8,5 +8,6 @@ const cors = require('cors');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 app.listen(PORT);
