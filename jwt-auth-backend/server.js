e4ee8fa8f35a4d3bd8feb8b9fb5ecc775cf63f4c
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-16 10:03:53 +0800

    Compare provided password with hashed password using bcrypt

--- jwt-auth-backend/server.js
+++ jwt-auth-backend/server.js
@@ -15,10 +15,12 @@ app.use(bodyParser.json());
 
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    
+
     const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
     const user = result.rows[0];
-    
-});
 
+    if (user && await bcrypt.compare(password, user.password)) {
+    };
+});
+    
 app.listen(PORT);
