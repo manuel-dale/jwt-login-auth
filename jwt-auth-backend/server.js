b6d6540035f329f4f7c9f9764f1ae967651877d0
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-16 09:49:46 +0800

    Refactor PORT variable

--- jwt-auth-backend/server.js
+++ jwt-auth-backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
