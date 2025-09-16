54ccf9f2ffee77434abe4418df4af1048e31269e
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-16 09:47:55 +0800

    Add bcrypt dependency

--- jwt-auth-backend/server.js
+++ jwt-auth-backend/server.js
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
 
 app.listen(PORT);
