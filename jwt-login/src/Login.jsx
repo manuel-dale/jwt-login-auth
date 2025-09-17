6d3a3f01234c554f92f38fcffa19f6806b1ea592
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-17 23:12:52 +0800

    Add section tag on log-in container

--- jwt-login/src/Login.jsx
+++ jwt-login/src/Login.jsx
@@ -16,28 +16,30 @@ function Login() {
   }, []);
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <section>
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </section>
   )
 };
 
