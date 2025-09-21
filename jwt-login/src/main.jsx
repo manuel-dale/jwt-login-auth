b00da36d8466b8bef592741ab4001a49d85c4a30
Manuel Dale <macatiag.manueldale@gmail.com>
2025-09-21 23:25:37 +0800

    Import react router component

--- jwt-login/src/main.jsx
+++ jwt-login/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 import { AuthProvider } from './AuthProvider.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
